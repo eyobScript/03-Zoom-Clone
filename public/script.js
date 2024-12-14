7afc3a28848a072ac8f67ce2b5a1669300432a08
eyobScript <eyobbmulugeta@gmail.com>
2024-12-14 12:30:46 +0300

    checkup

--- public/script.js
+++ public/script.js
@@ -1,51 +1,57 @@
-const socket = io('/');
+const socket = io("/");
 
 const myPeer = new Peer(undefined, {
-  path: '/peerjs',
-  host: '/',
-  port: '3000',
+  path: "/peerjs",
+  host: "/",
+  port: "3000",
 });
 
-const videoGrid = document.getElementById('video-grid');
-const myVideo = document.createElement('video');
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
 myVideo.muted = true;
 let myVideoStream;
 async function getUserMediaStream() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
-      });
-      myVideoStream = stream;
-      addVideoStream(myVideo, stream);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
 
-      socket.on('user-connected', (userId) => {
-        connectToNewUser(userId, stream);
-      });
 
-    } catch (err) {
-      console.log('Error accessing media devices:', err);
-    }
-  }
-  getUserMediaStream();
-
-  myPeer.on('open', (id) => {  
-    socket.emit('join-room', ROOM_ID, id);
+  myPeer.on('call', (call) => {
+    call.answer(stream); // Answer the call with the user's
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
   });
 
 
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  } catch (err) {
+    console.log("Error accessing media devices:", err);
+  }
+}
+getUserMediaStream();
 
- 
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
-
-  
-function addVideoStream(video, stream){
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => video.play());
-    videoGrid.append(video);
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => video.play());
+  videoGrid.append(video);
 }
 
-function connectToNewUser(userId){
-
-  console.log(userId);
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 }
